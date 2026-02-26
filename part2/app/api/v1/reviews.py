915c4561eda1aefc83c59a3db1de9d8c2bfa65d2
v-lmb <virginie.amamelia@gmail.com>
2026-02-26 10:54:53 +0100

    Add: comments

--- part2/app/api/v1/reviews.py
+++ part2/app/api/v1/reviews.py
@@ -13,9 +13,18 @@ review_model = api.model("ReviewInput", {
 
 @api.route("/")
 class ReviewList(Resource):
+    """
+    Ressources for the reviews collection
+    """
 
     @api.expect(review_model)
     def post(self):
+        """
+        Create a new review
+        Return: dict
+        code 201 if the review is created
+        code 400 if validation fails
+        """
         try:
             review = facade.create_review(api.payload)
             return review.to_dict(), 201
@@ -23,14 +32,30 @@ class ReviewList(Resource):
             return {"error": str(e)}, 400
 
     def get(self):
+        """
+        Retrieve all reviews
+        Return : list
+        code 200 with a list of all review disctionnaries
+        empty list if no reviews
+        """
         reviews = facade.get_all_reviews()
         return [r.to_dict() for r in reviews], 200
 
 
 @api.route("/<review_id>")
 class ReviewResource(Resource):
+    """
+    Ressource for a single review
+    """
 
     def get(self, review_id):
+        """
+        Retrieve a review with his ID
+        review_id : str, UUID of the review
+        Return: dict
+        code 200 with review data
+        code 404 if not found
+        """
         review = facade.get_review(review_id)
         if not review:
             return {"error": "Review not found"}, 404
@@ -38,12 +63,26 @@ class ReviewResource(Resource):
 
     @api.expect(review_model)
     def put(self, review_id):
+        """
+        Update existing review
+        review_id : str, UUID of the review
+        Return: dict
+        code 200 with review data
+        code 404 if not found
+        """
         review = facade.update_review(review_id, api.payload)
         if not review:
             return {"error": "Review not found"}, 404
         return {"message": "Review updated successfully"}, 200
 
     def delete(self, review_id):
+        """
+        Delete review by his ID
+        review_id : str, UUID of the review
+        Return: dict
+        code 200 with review data
+        code 404 if not found
+        """
         success = facade.delete_review(review_id)
         if not success:
             return {"error": "Review not found"}, 404
